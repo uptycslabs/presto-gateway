9cfc0aa904cc051abfaff5832904e179e0c508c1
ryadav <ryadav@uptycs.com>
2023-06-27 15:11:23 +0530

    resolving entry script error

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,4 +5,4 @@ if [[ -z $gid || -z $uid   ]]; then
     exit 1
 fi
 
-java  --add-opens java.base/java.lang=ALL-UNNAMED  --add-opens java.base/java.net=ALL-UNNAMED-jar /usr/app/gateway-ha-1.9.5-jar-with-dependencies.jar server /opt/uptycs/prestogateway/config/gateway-ha-config.yml
+java  --add-opens java.base/java.lang=ALL-UNNAMED  --add-opens java.base/java.net=ALL-UNNAMED -jar /usr/app/gateway-ha-1.9.5-jar-with-dependencies.jar server /opt/uptycs/prestogateway/config/gateway-ha-config.yml
